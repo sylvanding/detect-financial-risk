dd2a143bb3c3a0fe943261954f3d836bbee08983
Sylvan Ding <sylvanding@qq.com>
2021-11-30 01:02:48 +0800

    update notifications and documentation link

--- website/notification_generate.py
+++ website/notification_generate.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 def note_generate():
     return {'title': 'Notification',
-            'time': '11.15.2021',
-            'content': '你好，我是该系统的Developer。目前，网站正处于测试阶段，'
+            'time': '11.30.2021',
+            'content': '你好，我是该系统的Developer。目前，网站已完成测试，'
                        '基本实现了财务危机预测的功能，您可以通过Demo选择我们提供的公司数据。'
-                       '测试期结束后，我们将采用uWSGI+Nginx部署方案，'
-                       '撰写可行性研究分析报告与项目开发计划、需求分析报告、测试分析报告、软件工程文档等。'
-                       '还会在GitHub上发布项目代码，部署Demo页面，利用docsify生成完整的Documentation. '
-                       '由于时间原因，本人又要投入另一项目的研发中，后续计划如上，愿一切安好！——Sylvan Ding'}
+                       '我们采用uWSGI+Nginx部署方案，'
+                       '撰写了项目开发文档，'
+                       '还在GitHub上发布了源码。'
+                       '由于时间原因，本人又要投入另一项目的研发中，愿一切安好！——Sylvan Ding'}
